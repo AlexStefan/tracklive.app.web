6028de4938076a3fbde7fb27e264b2525212e2dc
buto <Mihai.bodnariu@buto.ro>
2018-10-31 15:50:00 +0200

    Changed UI
    Small changes on UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,30 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { AgmCoreModule } from '@agm/core';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
-    
+    SidebarComponent,
   ],
   imports: [
-
+    BrowserAnimationsModule,
     BrowserModule,
     FormsModule,
     HttpClientModule,
     AgmCoreModule.forRoot({
       apiKey:'AIzaSyBBiunwpFpc-TalohQlc8aD2Ms7rIyMwaE'
-    })
+    }),
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [ AppComponent ]
